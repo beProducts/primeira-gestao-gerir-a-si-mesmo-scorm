c43e5ef4a8eb4bcc7d37193e52e9923c35919c9d
Thiago Hayaci <tyhayaci@gmail.com>
2020-08-20 13:34:47 -0300

    Ajuste tempo inicial e final pages

--- src/views/pages/Analise.vue
+++ src/views/pages/Analise.vue
@@ -39,6 +39,7 @@ export default {
       timeSpentOnPage: 0,
       mainTimer: null,
       alarmTimer: null,
+      enterTime: null
     };
   },
   mounted() {
@@ -57,11 +58,13 @@ export default {
       vm.timeSpentOnPage = 0;
       vm.saveVisit(to.meta.pageId);
       vm.initTimer();
+      vm.enterTime = Date.now();
     });
   },
   beforeRouteLeave(to, from, next) {
     this.stopTimer();
     this.setUserExit(from.meta.pageId, this.timeSpentOnPage);
+    this.setTimeScorm( Date.now() - this.enterTime );
     next();
   },
   methods: {
